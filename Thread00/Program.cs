9415a8e09a5f09959165dba685d1104077e2ba96
DIEGO PASSOS COSTA <080101286@prof.uninassau.edu.br>
2019-11-12 21:13:25 -0300

    inictal commit

---
+++ Thread00/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaThread0 = new Thread(acaoAlo);
+            minhaThread0.Start();
+
+            var minhaThread1 = new Thread(acaoTchau);
+            minhaThread1.Start();
+        }
+
+        public static void acaoAlo()
+        {
+            do
+            {
+                Console.WriteLine("alo");
+                Thread.Sleep(1000);
+
+            } while (true);
+
+        }
+
+        public static void acaoTchau()
+        {
+            do
+            {
+
+                Console.WriteLine("tchau");
+                Thread.Sleep(2000);
+            } while (true);
+        }
+
+    }
+}
